512e04375fbe6a19243eada124ec2edaa2e83f7c
Osama <97450706+osama2-9@users.noreply.github.com>
2024-06-09 00:13:59 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{res.send("hi api")})
+
 
 cloudinary.config({
   cloud_name: process.env.C_C_NAME,
@@ -21,7 +21,7 @@ cloudinary.config({
   api_secret: process.env.C_API_SECRET,
 });
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
